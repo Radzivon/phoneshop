ca1162d6cf91891a9811375f5480658fe49bd188
uladradzivon@gmail.com <uladradzivon@gmail.com>
2019-10-30 09:12:17 +0300

    task 2.1 and 2.2

--- core/src/main/java/com/es/core/model/phone/Color.java
+++ core/src/main/java/com/es/core/model/phone/Color.java
@@ -1,8 +1,24 @@
 package com.es.core.model.phone;
 
+import java.util.Objects;
+
 public class Color {
     private Long id;
     private String code;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return Objects.equals(id, color.id) &&
+                Objects.equals(code, color.code);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, code);
+    }
 
     public Long getId() {
         return id;
--- core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
+++ core/src/main/java/com/es/core/model/phone/JdbcPhoneDao.java
@@ -1,27 +1,70 @@
 package com.es.core.model.phone;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Component
-public class JdbcPhoneDao implements PhoneDao{
+public class JdbcPhoneDao implements PhoneDao {
+    private static final String SQL_SAVE_PHONE =
+            "insert into phones (brand, model, price, displaySizeInches, weightGr, lengthMm, widthMm, heightMm, announced, deviceType, os, displayResolution, pixelDensity, displayTechnology, backCameraMegapixels, frontCameraMegapixels, ramGb, internalStorageGb, batteryCapacityMah, talkTimeHours, standByTimeHours, bluetooth, positioning, imageUrl, description)" +
+                    "values " +
+                    "(:brand, :model, :price, :displaySizeInches, :weightGr, :lengthMm, :widthMm, :heightMm, :announced, :deviceType, :os, :displayResolution, :pixelDensity, :displayTechnology, :backCameraMegapixels, :frontCameraMegapixels, :ramGb, :internalStorageGb, :batteryCapacityMah, :talkTimeHours, :standByTimeHours, :bluetooth, :positioning, :imageUrl, :description)";
+    private static final String SQL_SAVE_COLORS = "insert into phone2color (code, id) values (?, ?)";
+    private static final String SQL_GET_PHONES_WITH_COLORS = "select * from colors where id in (select * from phone2color where phoneId = ?)";
+    private static final String SQL_GET_ALL_PHONES_WITH_COLORS = "select * from city c left join brand n on c.id = n.id order by brand";
     @Resource
     private JdbcTemplate jdbcTemplate;
 
     public Optional<Phone> get(final Long key) {
-        throw new UnsupportedOperationException("TODO");
+        List<Phone> phones = jdbcTemplate.query(SQL_GET_PHONES_WITH_COLORS, new Object[]{key}, new PhoneResultSetExtractor());
+        if (phones.isEmpty()) {
+            throw new NoSuchElementException("Don't have phone with id " + key);
+        }
+        return Optional.of(phones.get(0));
     }
 
     public void save(final Phone phone) {
-        throw new UnsupportedOperationException("TODO");
+        if (phone == null) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+        Set<Color> colors = phone.getColors();
+
+        colors.stream().forEach(color -> jdbcTemplate.update(SQL_SAVE_COLORS, color.getId(), color.getCode()));
+
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("brand", phone.getBrand());
+        paramMap.put("model", phone.getModel());
+        paramMap.put("price", phone.getPrice());
+        paramMap.put("displaySizeInches", phone.getDisplaySizeInches());
+        paramMap.put("weightGr", phone.getWeightGr());
+        paramMap.put("lengthMm", phone.getLengthMm());
+        paramMap.put("widthMm", phone.getWidthMm());
+        paramMap.put("heightMm", phone.getHeightMm());
+        paramMap.put("announced", phone.getAnnounced());
+        paramMap.put("deviceType", phone.getDeviceType());
+        paramMap.put("os", phone.getOs());
+        paramMap.put("displayResolution", phone.getDisplayResolution());
+        paramMap.put("pixelDensity", phone.getPixelDensity());
+        paramMap.put("displayTechnology", phone.getDisplayTechnology());
+        paramMap.put("backCameraMegapixels", phone.getBackCameraMegapixels());
+        paramMap.put("frontCameraMegapixels", phone.getFrontCameraMegapixels());
+        paramMap.put("ramGb", phone.getRamGb());
+        paramMap.put("internalStorageGb", phone.getInternalStorageGb());
+        paramMap.put("batteryCapacityMah", phone.getBatteryCapacityMah());
+        paramMap.put("talkTimeHours", phone.getTalkTimeHours());
+        paramMap.put("standByTimeHours", phone.getStandByTimeHours());
+        paramMap.put("bluetooth", phone.getBluetooth());
+        paramMap.put("positioning", phone.getPositioning());
+        paramMap.put("imageUrl", phone.getImageUrl());
+        paramMap.put("description", phone.getDescription());
+
+        jdbcTemplate.update(SQL_SAVE_PHONE, paramMap);
     }
 
     public List<Phone> findAll(int offset, int limit) {
-        return jdbcTemplate.query("select * from phones offset " + offset + " limit " + limit, new BeanPropertyRowMapper(Phone.class));
+        return jdbcTemplate.query(SQL_GET_ALL_PHONES_WITH_COLORS + " offset " + offset + " limit " + limit, new PhoneResultSetExtractor());
     }
 }
